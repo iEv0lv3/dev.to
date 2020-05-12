19007370df3bf6b8cce79e5984896a83aa408721
Karen Nguyen <knguyen4184@yahoo.com>
2020-05-12 16:30:25 -0600

    Fix linter error

--- app/javascript/dashboard/dashboard.jsx
+++ app/javascript/dashboard/dashboard.jsx
@@ -1,4 +1,8 @@
 import { h, Component } from 'preact';
+import displayRead from '../../assets/images/display_read.png';
+import displayClick from '../../assets/images/display_click.png';
+import displayTime from '../../assets/images/display_time.png';
+import displayComment from '../../assets/images/display_comment.png';
 
 export class Dashboard extends Component {
   constructor(props) {
@@ -10,14 +14,28 @@ export class Dashboard extends Component {
     /* eslint-disable */ const { visits, comments } = this.props;
 
     return (
-      <div>
-        <div className="user-visits">
-          User visits:
-          {visits}
+      <div className="display-div">
+        <div className="user-visits stats">
+          VISITS
+          <img className="icons" src={displayClick}></img>
+          {visits} total
         </div>
-        <div className="comments">
-          Comment count:
-          {comments}
+        <div className="comments stats">
+          COMMENTS
+          <img className="icons" src={displayComment}></img>
+          {comments} total
+        </div>
+        <div className="time stats">
+          TIME SPENT
+          {/* {comments} */}
+          <img className="icons" src={displayTime}></img>
+        </div>
+        <div className="articles stats">
+          <div>
+            ARTICLES READ
+            <img className="icons" src={displayRead}></img>
+          </div>
+          <div>WORDS READ</div>
         </div>
       </div>
     );
