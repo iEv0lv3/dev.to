737f05d2a1b355fefc999e55bb051bc7cd061444
Edward Stoner <edwardlstoner@gmail.com>
2020-05-15 08:03:21 -0600

    Add more structure for dashboards

--- app/javascript/dashboard/dashboard.jsx
+++ app/javascript/dashboard/dashboard.jsx
@@ -25,30 +25,61 @@ export class Dashboard extends Component {
       <div className="display-div">
         <div className="user-visits stats">
           <div className="title">VISITS</div>
-          <img className="icons" src={displayClick}></img>
-          {visits} total
-          {visit} avg/day
+          <div className="stat-row">
+            <img className="icons" src={displayClick}></img>
+            <div className="stats-group">
+              <div className="stat-display stat-total">
+                <div className="display-big">{visits}</div> total
+              </div>
+              <div className="stat-display">
+                <div className="display-big">{visit}</div> avg/day
+              </div>
+            </div>
+          </div>
         </div>
         <div className="comments stats">
           <div className="title">COMMENTS</div>
-          <img className="icons" src={displayComment}></img>
-          {comments} total
+          <div className="stat-row">
+            <img className="icons" src={displayComment}></img>
+            <div className="stat-display">
+              <div className="display-big">{comments}</div> total
+            </div>
+          </div>
         </div>
         <div className="time stats">
           <div className="title">TIME SPENT</div>
-          <img className="icons" src={displayTime}></img>
-          {time} per day
+          <div className="stat-row">
+            <img className="icons" src={displayTime}></img>
+            <div className="stat-display">
+              <div className="display-big">{time}</div> min/day
+            </div>
+          </div>
         </div>
         <div className="articles">
-          <div className="border">
-            ARTICLES READ
-            <img className="icons article-icon" src={displayRead}></img>
-            {articles} total
-            {avgArticles} avg/day
+          <div className="stats double">
+            <div className="title">ARTICLES READ</div>
+            <div className="stat-row">
+              <img className="icons article-icon" src={displayRead}></img>
+              <div className="stat-display stat-total">
+                <div className="display-big">{articles}</div> total
+              </div>
+              <div className="stat-display">
+                <div className="display-big">{avgArticles}</div> avg/day
+              </div>
+            </div>
           </div>
-          WORDS READ
-          {words} total
-          {avgWords} avg/day
+          <div className="divider"></div>
+          <div className="stats double">
+            <div className="title">WORDS READ</div>
+            <div className="stat-row">
+              <div className="stat-display stat-total">
+                <div className="display-big">{words}</div> total
+              </div>
+              <div className="stat-display">
+                <div className="display-big">{avgWords}</div> avg/day
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
